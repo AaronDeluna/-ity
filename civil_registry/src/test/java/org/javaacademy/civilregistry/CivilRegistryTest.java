9fc8141531725c89bcc36b5afb055a4f63f66905
Gennadiy Bakulin <g.bakulin@yandex.ru>
2024-10-26 20:16:09 +0700

    добавил тестовую папку и плагин junit

---
+++ civil_registry/src/test/java/org/javaacademy/civilregistry/CivilRegistryTest.java
@@ -0,0 +1,32 @@
+package org.javaacademy.civilregistry;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class CivilRegistryTest {
+
+  @BeforeEach
+  void setUp() {
+  }
+
+  @AfterEach
+  void tearDown() {
+  }
+
+  @Test
+  void birthOfChild() {
+  }
+
+  @Test
+  void registrationMarriage() {
+  }
+
+  @Test
+  void registrationDivorce() {
+  }
+
+  @Test
+  void statisticsForDate() {
+  }
+}
