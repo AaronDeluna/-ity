a853a508573e2e1d25595323d1f49c21fa151d04
Gennadiy Bakulin <g.bakulin@yandex.ru>
2024-10-26 14:17:09 +0700

    правка кода в соответствии с чекстайлом

--- human/src/main/java/org/javaacademy/human/Gender.java
+++ human/src/main/java/org/javaacademy/human/Gender.java
@@ -4,12 +4,16 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 
+/**
+ * Пол человека (мужчина/женщина).
+ */
+
 @Getter
 @RequiredArgsConstructor
 @FieldDefaults(makeFinal = true)
 public enum Gender {
-    MALE("Мужской"),
-    FEMALE("Женский");
+  MALE("Мужской"),
+  FEMALE("Женский");
 
-    String description;
+  String description;
 }
--- human/src/main/java/org/javaacademy/human/Human.java
+++ human/src/main/java/org/javaacademy/human/Human.java
@@ -1,97 +1,112 @@
 package org.javaacademy.human;
 
+import java.util.ArrayList;
+import java.util.List;
 import lombok.Getter;
 import lombok.NonNull;
 import org.apache.commons.lang3.StringUtils;
 
-import java.util.ArrayList;
-import java.util.List;
-
+/**
+ * Класс Человек.
+ */
 @Getter
 public class Human {
-    String name;
-    String surname;
-    String patronymic;
-    final Gender gender;
-    Human father;
-    Human mother;
-    final List<Human> children = new ArrayList<>();
 
-    public Human(@NonNull String name, @NonNull String surname, @NonNull String patronymic, @NonNull Gender gender) {
-        setName(name);
-        setSurname(surname);
-        setPatronymic(patronymic);
-        this.gender = gender;
+  String name;
+  String surname;
+  String patronymic;
+  final Gender gender;
+  Human father;
+  Human mother;
+  final List<Human> children = new ArrayList<>();
+
+  /**
+   * Конструктор принимает параметры и инициализирует объект класса Человек.
+   *
+   * @param name       - имя человека.
+   * @param surname    - фамилия человека.
+   * @param patronymic - отчество человека.
+   * @param gender     - пол человека.
+   */
+
+  public Human(@NonNull String name, @NonNull String surname, @NonNull String patronymic,
+      @NonNull Gender gender) {
+    setName(name);
+    setSurname(surname);
+    setPatronymic(patronymic);
+    this.gender = gender;
+  }
+
+  /**
+   * Функция преобразования текста (первая заглавная буква, все остальные буквы в нижнем регистре).
+   */
+
+  private String capitalize(String text) {
+    return StringUtils.capitalize(text.toLowerCase());
+  }
+
+  /**
+   * Функция проверки строк на пустой ввод - наличие только пробелов или ни одного символа.
+   */
+
+  private void checkIsEmptyString(String str) {
+    if (str.isBlank()) {
+      throw new IllegalArgumentException(
+          "Поля не должны быть пустыми или состоять только из пробелов.");
+    }
+  }
+
+  private void setName(String name) {
+    checkIsEmptyString(name);
+    this.name = capitalize(name);
+  }
+
+  private void setSurname(String surname) {
+    checkIsEmptyString(surname);
+    this.surname = capitalize(surname);
+  }
+
+  private void setPatronymic(String patronymic) {
+    checkIsEmptyString(patronymic);
+    this.patronymic = capitalize(patronymic);
+  }
+
+  /**
+   * Функция указания родителей.
+   */
+
+  public void setParents(@NonNull Human father, @NonNull Human mother) {
+    this.father = father;
+    this.mother = mother;
+    father.getChildren().add(this);
+    mother.getChildren().add(this);
+  }
+
+  /**
+   * Функция, которая отвечает за производство ребенка на свет.
+   */
+
+  public Human produceChild(@NonNull String name, @NonNull String surname,
+      @NonNull String patronymic,
+      @NonNull Gender gender, @NonNull Human secondParent) {
+    if (this.gender == secondParent.gender) {
+      throw new IllegalArgumentException("Родители должны быть противоположных полов!");
     }
 
-    /**
-     * Функция преобразования текста (первая заглавная буква, все остальные буквы в нижнем регистре).
-     */
-
-    private String capitalize(String text) {
-        return StringUtils.capitalize(text.toLowerCase());
+    Human child = new Human(name, surname, patronymic, gender);
+    if (this.gender == Gender.MALE) {
+      child.setParents(this, secondParent);
+    } else {
+      child.setParents(secondParent, this);
     }
+    return child;
+  }
 
-    /**
-     * Функция проверки строк на пустой ввод - наличие только пробелов или ни одного символа.
-     */
+  /**
+   * Функция получения полного имени.
+   */
 
-    private void checkIsEmptyString(String str) {
-        if (str.isBlank()) {
-            throw new IllegalArgumentException("Поля не должны быть пустыми или состоять только из пробелов.");
-        }
-    }
-
-    private void setName(String name) {
-        checkIsEmptyString(name);
-        this.name = capitalize(name);
-    }
-
-    private void setSurname(String surname) {
-        checkIsEmptyString(surname);
-        this.surname = capitalize(surname);
-    }
-
-    private void setPatronymic(String patronymic) {
-        checkIsEmptyString(patronymic);
-        this.patronymic = capitalize(patronymic);
-    }
-
-    /**
-     * Функция указания родителей.
-     */
-
-    public void setParents(@NonNull Human father, @NonNull Human mother) {
-        this.father = father;
-        this.mother = mother;
-        father.getChildren().add(this);
-        mother.getChildren().add(this);
-    }
-
-    /**
-     * Функция, которая отвечает за производство ребенка на свет.
-     */
-
-    public Human produceChild(@NonNull String name, @NonNull String surname, @NonNull String patronymic,
-                              @NonNull Gender gender, @NonNull Human secondParent) {
-        if (this.gender == secondParent.gender) {
-            throw new IllegalArgumentException("Родители должны быть противоположных полов!");
-        }
-
-        Human child = new Human(name, surname, patronymic, gender);
-        if (this.gender == Gender.MALE) {
-            child.setParents(this, secondParent);
-        } else {
-            child.setParents(secondParent, this);
-        }
-        return child;
-    }
-
-    /**
-     * Функция получения полного имени.
-     */
-
-    public String getFullName() {
-        return String.format("%s %s %s", surname, name, patronymic);
-    }
+  public String getFullName() {
+    return String.format("%s %s %s", surname, name, patronymic);
+  }
 }
