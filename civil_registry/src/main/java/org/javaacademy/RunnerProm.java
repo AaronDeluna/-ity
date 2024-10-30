5739e7bf2a0a4b8774c38ea8c835564b68e503f8
Gennadiy Bakulin <g.bakulin@yandex.ru>
2024-10-30 22:30:30 +0700

    правка Runner в загсе

--- civil_registry/src/main/java/org/javaacademy/RunnerProm.java
+++ civil_registry/src/main/java/org/javaacademy/RunnerProm.java
@@ -18,11 +18,18 @@ public class RunnerProm {
    * @param args аргументы при запуске программы.
    */
   public static void main(String[] args) {
-    CivilRegistry civilRegistry = new CivilRegistry("TEST_ZAGS");
+    CivilRegistry civilRegistry = new CivilRegistry(args[0]);
+
     Citizen citizen1 = new Citizen("александр", "пушкин", "СЕргеевич", MALE);
     Citizen citizen2 = new Citizen("ЕЛЕНА", "петрова", "Петровна", FEMALE);
     Citizen citizen3 = new Citizen("Антонина  ", " Иванова", "НикоЛаевна", FEMALE);
     Citizen citizen4 = new Citizen("Сергей  ", " Есенин", "петрович", MALE);
+
+    civilRegistry.registrationMarriage(citizen1, citizen2, LocalDate.now());
+    civilRegistry.registrationMarriage(citizen3, citizen4, LocalDate.now());
+
+    civilRegistry.registrationDivorce(citizen1, citizen2, LocalDate.now());
+
     Citizen child1 = citizen1.produceChild("Антон  ",
         " пушкин", "александрович", MALE, citizen2);
     Citizen child2 = citizen4.produceChild("Юлия  ", "Иванова",
@@ -30,17 +37,9 @@ public class RunnerProm {
     Citizen child3 = citizen1.produceChild("Оксана  ", "Иванова",
         "Андреевна", FEMALE, citizen2);
 
-    civilRegistry.registrationMarriage(citizen1, citizen2, LocalDate.now());
-    civilRegistry.registrationDivorce(citizen1, citizen2, LocalDate.now());
-    civilRegistry.registrationMarriage(citizen3, citizen4, LocalDate.now());
-    //civilRegistry.registrationMarriage(citizen3, citizen4, LocalDate.now());
-    //civilRegistry.registrationDivorce(citizen3, citizen4, LocalDate.now());
-    civilRegistry.registrationDivorce(citizen3, citizen4, LocalDate.now());
     civilRegistry.birthOfChild(child1, citizen1, citizen2, LocalDate.now());
     civilRegistry.birthOfChild(child3, citizen1, citizen2, LocalDate.now());
-
-    civilRegistry.birthOfChild(child2, citizen3, citizen4, LocalDate.now());
-    //civilRegistry.birthOfChild(child2, citizen3, citizen1, LocalDate.now());
+    civilRegistry.birthOfChild(child2, citizen4, citizen3, LocalDate.now());
 
     civilRegistry.statisticsForDate(LocalDate.now());
   }
